dfe6c78e7d5e9d8ee0bdfcdcd2602325455251a4
avadavelli <anand.vadavelli@viasat.com>
2021-03-21 23:37:22 -0700

    Fixed connectToDb

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 /* eslint linebreak-style: ["error", "windows"] */
-const fs = require('fs');
-require('dotenv').config();
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { MongoClient } = require('mongodb');
+const fs = require("fs");
+require("dotenv").config();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { MongoClient } = require("mongodb");
 
 // const url = "mongodb://localhost/issuetracker";
 
@@ -14,7 +14,7 @@ const port = process.env.API_SERVER_PORT || 3000;
 // const url = 'mongodb://UUU:PPP@XXX.mlab.com:33533/issuetracker';
 
 let db;
-let aboutMessage = 'Inventory Management 1.0';
+let aboutMessage = "Inventory Management 1.0";
 
 const resolvers = {
   Query: {
@@ -30,18 +30,18 @@ const resolvers = {
 const productDB = [];
 
 async function productList() {
-  const products = await db.collection('products').find({}).toArray();
+  const products = await db.collection("products").find({}).toArray();
   return products;
   // return productDB;
 }
 
 async function getNextSequence(name) {
   const result = await db
-    .collection('counters')
+    .collection("counters")
     .findOneAndUpdate(
       { _id: name },
       { $inc: { current: 1 } },
-      { returnOriginal: false },
+      { returnOriginal: false }
     );
   return result.value.current;
 }
@@ -54,23 +54,26 @@ async function productAdd(_, { product }) {
   // product.id = productDB.length + 1;
   // productDB.push(product);
   // return product;
-  product.id = await getNextSequence('products');
-  const result = await db.collection('products').insertOne(product);
+  product.id = await getNextSequence("products");
+  const result = await db.collection("products").insertOne(product);
   const savedProduct = await db
-    .collection('products')
+    .collection("products")
     .findOne({ _id: result.insertedId });
   return savedProduct;
 }
 
 async function connectToDb() {
-  const client = new MongoClient(url, { useNewUrlParser: true });
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await client.connect();
-  console.log('Connected to MongoDB at', url);
+  console.log("Connected to MongoDB at", url);
   db = client.db();
 }
 
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync("schema.graphql", "utf-8"),
   resolvers,
   formatError: (error) => {
     console.log(error);
@@ -79,7 +82,7 @@ const server = new ApolloServer({
 });
 
 const app = express();
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: "/graphql" });
 
 // app.listen(3000, function () {
 //   console.log("App started on port 3000");
@@ -92,6 +95,6 @@ server.applyMiddleware({ app, path: '/graphql' });
       console.log(`API server started on port ${port}`);
     });
   } catch (err) {
-    console.log('ERROR:', err);
+    console.log("ERROR:", err);
   }
-}());
+})();
